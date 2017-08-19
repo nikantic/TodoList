43c3898b88a955354f2fc941f7ff7314c56d16ef
Nikola Antić <nikantic@outlook.com>
2017-08-19 23:46:06 +0200

    redesign, fix functionalities

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,19 +1,29 @@
 // Check off specific To Dos by clicking
 $("ul").on("click", "li", function() {
 	if ($(this).hasClass("completed")) {
-		$(this).removeClass("completed");
-		$(this).insertBefore($('.completed:first'))
+		$(this).find(".checkIcon").removeClass("fa-check-square").addClass("fa-square-o");
+		setTimeout(function(){
+			$(this).removeClass("completed");
+			$(this).insertBefore($('.completed:first'))
+		}.bind(this), 200);
 	} else if ($("li").hasClass("completed")) {
-		$(this).insertBefore($('.completed:first'));
-		$(this).addClass("completed");
+		//checking if any li has class .completed
+		$(this).find(".checkIcon").removeClass("fa-square-o").addClass("fa-check-square");
+		setTimeout(function(){
+			$(this).insertBefore($('.completed:first'));
+			$(this).addClass("completed");
+		}.bind(this), 200);
 	} else {
-		$(this).addClass("completed");
-		$(this).appendTo("ul");
+		$(this).find(".checkIcon").removeClass("fa-square-o").addClass("fa-check-square");
+		setTimeout(function(){
+			$(this).addClass("completed");
+			$(this).appendTo($("ul"));
+		}.bind(this), 200);
 	}
 });
 
 // Click on X to delete To Dos
-$("ul").on("click", "span", function(event) {
+$("ul").on("click", ".removeButton", function(event) {
 	$(this).parent().fadeOut(500, function() { 
 		$(this).remove();
 	});
@@ -26,7 +36,7 @@ $("input[type='text']").keypress(function(event) {
 		var todoText = $(this).val();
 		$(this).val("");
 		// create a new li and add to ul
-		$("ul").prepend("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> " + todoText + "</li>")
+		$("ul").prepend("<li><span class='checkButton'><i class='fa fa-square-o checkIcon' aria-hidden='true'></i></span><span class='removeButton'><i class='fa fa-remove' aria-hidden='true'></i></span> " + todoText + "</li>")
 	}
 });
 
