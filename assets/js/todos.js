3c151b6b7b3324bf7a71f2182897b1d35a351742
Nikola Antić <nikantic@outlook.com>
2017-08-20 01:03:18 +0200

    new addButton, animations, input checks

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -30,16 +30,31 @@ $("ul").on("click", ".removeButton", function(event) {
 	event.stopPropagation();
 });
 
+$("input[type='text']").keyup(function() {
+	if($(this).val() < 1) {
+		$(".addButton").css({"opacity": 0, "pointer-events": "none"});
+	} else {
+		$(".addButton").css({"opacity": 1, "pointer-events": "all"});
+	}
+});
+
 $("input[type='text']").keypress(function(event) {
 	if (event.which === 13) {
 		// grab new todo text from input
 		var todoText = $(this).val();
-		$(this).val("");
-		// create a new li and add to ul
-		$("ul").prepend("<li><span class='checkButton'><i class='fa fa-square-o checkIcon' aria-hidden='true'></i></span><span class='removeButton'><i class='fa fa-remove' aria-hidden='true'></i></span> " + todoText + "</li>")
+		if (todoText.length > 0) {
+			$(this).val("");
+			// create a new li and add to ul
+			$("ul").prepend("<li><span class='checkButton'><i class='fa fa-square-o checkIcon' aria-hidden='true'></i></span><span class='removeButton'><i class='fa fa-remove' aria-hidden='true'></i></span> " + todoText + "</li>");
+			$("li:first").addClass("blinkAnim");
+		}		
 	}
 });
 
-$(".fa-plus").click(function() {
-	$("input[type='text']").fadeToggle(); 
+$(".addButton").click(function() {
+	var todoText = $("input[type='text']").val();
+	$("input[type='text']").val("");
+	$("ul").prepend("<li><span class='checkButton'><i class='fa fa-square-o checkIcon' aria-hidden='true'></i></span><span class='removeButton'><i class='fa fa-remove' aria-hidden='true'></i></span> " + todoText + "</li>");
+	$("li:first").addClass("blinkAnim");
+	$(this).css({"opacity": 0, "pointer-events": "none"});
 });
